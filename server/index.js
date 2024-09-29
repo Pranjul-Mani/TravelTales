c1bfe15845fb91d27ed2c0b133d59c2b28f4d901
Pranjul-Mani <132186863+Pranjul-Mani@users.noreply.github.com>
2024-09-29 16:47:23 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -12,13 +12,23 @@ dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
+app.use(cors(
+    {
+        origin:[""],
+        methods:["POST", "GET"],
+        credentials:true
+    }
+));
 
 app.use('/posts', postRoutes);
 app.use('/user',userRoutes);
 
 // const CONNECTION_URL = 'mongodb+srv://pranjul:pran123@cluster0.5ny4f6x.mongodb.net/?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
+
+app.get("/",(req,res) =>{
+    res.json("hello");
+})
 
 mongoose.connect(process.env.CONNECTION_URL)
     .then(() => app.listen(PORT, () => console.log(`Server running on port : ${PORT}`)))
