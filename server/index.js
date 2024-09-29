eb97b9a0b2f860184ff9b3e39d63b6397bc6550a
Pranjul Mani <pranjulmani010@gmail.com>
2024-09-29 19:52:34 +0530

    new file

--- server/index.js
+++ server/index.js
@@ -12,23 +12,14 @@ dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors(
-    {
-        origin:[""],
-        methods:["POST", "GET"],
-        credentials:true
-    }
-));
+app.use(cors());
 
 app.use('/posts', postRoutes);
-app.use('/user',userRoutes);
+app.use('/user', userRoutes);
 
 // const CONNECTION_URL = 'mongodb+srv://pranjul:pran123@cluster0.5ny4f6x.mongodb.net/?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
-app.get("/",(req,res) =>{
-    res.json("hello");
-})
 
 mongoose.connect(process.env.CONNECTION_URL)
     .then(() => app.listen(PORT, () => console.log(`Server running on port : ${PORT}`)))
